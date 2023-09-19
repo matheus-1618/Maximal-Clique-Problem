ba96a01734e9621005bc4df0104cf7822aca8351
matheus-1618 <matheus1207live@gmail.com>
2023-09-19 18:45:14 -0300

    Add first implementation

---
+++ src/get_matrix.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Função para ler o graph a partir do arquivo de entrada
+vector<vector<int>> ReadGraph(const std::string& fileName, int& numVertex) {
+    ifstream file(fileName);
+    int numEdges;
+    file >> numVertex >> numEdges;
+
+    vector<vector<int>> graph(numVertex, vector<int>(numVertex, 0));
+
+    for (int i = 0; i < numEdges; ++i) {
+        int u, v;
+        file >> u >> v;
+        graph[u - 1][v - 1] = 1;
+        graph[v - 1][u - 1] = 1;  // O graph é não direcionado
+    }
+
+    file.close();
+
+    return graph;
+}
+
+vector<int> FindMaximalClique(vector<vector<int>>& graph, int numVertex) {
+    vector<int> MaximalClique;
+    vector<int> candidates;
+
+    // Inicialmente, todos os nós são candidates
+    for (int i = 0; i < numVertex; ++i) {
+        candidates.push_back(i);
+    }
+
+    while (!candidates.empty()) {
+        int v = candidates.back();
+        candidates.pop_back();
+        bool canAdd = true;
+
+        for (int u : MaximalClique) {
+            if (graph[u][v] == 0) {
+                canAdd = false;
+                break;
+            }
+        }
+
+        if (canAdd) {
+            MaximalClique.push_back(v);
+            vector<int> newCandidates;
+
+            for (int u : candidates) {
+                bool adjacentToAll = true;
+
+                for (int c : MaximalClique) {
+                    if (graph[u][c] == 0) {
+                        adjacentToAll = false;
+                        break;
+                    }
+                }
+
+                if (adjacentToAll) {
+                    newCandidates.push_back(u);
+                }
+            }
+
+            candidates = newCandidates;
+        }
+    }
+
+    return MaximalClique;
+}
+
+int main() {
+    int numVertex = 1000;
+    vector<vector<int>> grafo;
+
+    grafo = ReadGraph("grafo.txt", numVertex);
+    vector<int> maximalClique = FindMaximalClique(grafo, numVertex);
+
+    cout << "Maximal Clique: ";
+    for (int v : maximalClique) {
+        cout << v << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
