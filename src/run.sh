cb9eda209cefd5c8a6f00ebdd9c3f5ac84910fad
matheus-1618 <matheus1207live@gmail.com>
2023-10-08 13:17:27 -0300

    Add recursive solution

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,6 @@ g++ -Wall -O3 -g implementations/0_search_clique.cpp -o 0_search_clique
 ./0_search_clique
 g++ -Wall -O3 -g implementations/1_edges_heuristic.cpp -o 1_edges_heuristic
 ./1_edges_heuristic
+g++ -Wall -O3 -g implementations/2_recursive_clique.cpp -o 2_recursive_clique
+./2_recursive_clique
 python3 python/verify_clique.py
